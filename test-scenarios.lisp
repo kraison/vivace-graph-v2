37d66da44448fa50e4788efd5e0b96cfa3ec123f
Kevin Raison <raison@chatsubo.net>
2011-02-28 17:35:09 -0800

    slight modification to test scenarios

--- test-scenarios.lisp
+++ test-scenarios.lisp
@@ -11,17 +11,21 @@
 			 (format t "~%basic-concurrency-1: ~A: ~A~%" 
 				 (triple-id triple) triple)
 			 (setq *basic-concurrency-1* triple)
-			 (sleep 8))))))
-	(thr2 (make-thread #'(lambda ()
-			       (sleep 11)
-			       (let ((triple (lookup-triple 
-					      "This" "is-a" "test" "VGT")))
-				 (format t "basic-concurrency-1 lookup: ~A~%" triple)
-				 (if (triple-equal triple *basic-concurrency-1*)
-				     (setq *basic-concurrency-1* triple)
-				     (setq *basic-concurrency-1* nil)))))))
+			 (sleep 3))))))
+	  (thr2 (make-thread 
+		 #'(lambda ()
+		     (sleep 1)
+		     (let ((triple (add-triple
+				    "This" "is-a" "test" :graph "VGT")))
+		       (if (triple? triple)
+			   (format t "basic-concurrency-1 lookup: ~A: ~A~%" 
+				   (triple-id triple) triple)
+			   (format t "basic-concurrency-1 lookup: ~A~%" triple))
+			   (if (triple-equal triple *basic-concurrency-1*)
+			       (setq *basic-concurrency-1* triple)
+			       (setq *basic-concurrency-1* nil)))))))
       (join-thread thr1)
       (join-thread thr2)
-      (format t "~%basic-concurrency-1: ~A~%" *basic-concurrency-1*)
+      (format t "basic-concurrency-1: ~A~%" *basic-concurrency-1*)
       *basic-concurrency-1*)))
     
