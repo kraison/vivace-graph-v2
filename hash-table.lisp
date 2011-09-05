9f8b4bca7e28807400e7d5134b10f7309ff3cda7
MON KEY <monkey@sandpframing.com>
2011-09-05 16:40:20 -0400

    * sb-thread.lisp: No longer define `my-get-spinlock' in the package "SB-THREAD". No longer export `get-spinlock', `release-spinlock', `spinlock-value' from the package "SB-THREAD" its not our namespace.   (vg-get-spinlock): Renamed from `my-get-spinlock'. Now explicitly evaluate symbols with their fully SBCL package names, e.g. "SB-THREAD", "SB-SYS", etc.   (vg-current-thread): New Function. with reader conditional for #-sbcl wraps bordeaux-threads:current-thread.   (vg-current-thread-spinlock-value-check): New Function. Abstraction for the `sb-thread:spinlock-value' eq checks that occur in `acquire-hash-table-lock' and `release-hash-table-lock' in vivace-graph-v2/hash-table.lisp
    * hash-table.lisp (acquire-hash-table-lock): No longer directly checks value of sb-thread:*current-thread* against `my-get-spinlock' now uses `vg-current-thread-spinlock-value-check' instead.
      (release-hash-table-lock): No longer directly checks value of sb-thread:*current-thread* against `my-get-spinlock' now uses `vg-current-thread-spinlock-value-check' instead.

--- hash-table.lisp
+++ hash-table.lisp
@@ -1,13 +1,13 @@
 (in-package #:vivace-graph-v2)
 
 (defun acquire-hash-table-lock (hash-table)
-  (if (eq sb-thread:*current-thread* 
-	  (sb-thread:spinlock-value (sb-impl:hash-table-spinlock hash-table)))
+    ;; (if (eq sb-thread:*current-thread* 
+  (if (vg-current-thread-spinlock-value-check)
       t
-      (sb-thread:my-get-spinlock (sb-impl:hash-table-spinlock hash-table))))
+      ;; :WAS (sb-thread:my-get-spinlock (sb-impl:hash-table-spinlock hash-table))))
+      (vg-get-spinlock (sb-impl:hash-table-spinlock hash-table))))
 
 (defun release-hash-table-lock (hash-table)
-  (if (eq sb-thread:*current-thread* 
-	  (sb-thread:spinlock-value (sb-impl:hash-table-spinlock hash-table)))
-      (sb-thread:release-spinlock (sb-impl:hash-table-spinlock hash-table))))
-
+  ;; (if (eq sb-thread:*current-thread* 
+  (if (vg-current-thread-spinlock-value-check)
+      (sb-thread::release-spinlock (sb-impl:hash-table-spinlock hash-table))))
--- templates.lisp
+++ templates.lisp
@@ -9,14 +9,14 @@ make-v4-uuid?
 
 ;; (string-downcase (princ-to-string (uuid:make-v4-uuid)))
 ;;=> "a8c5c06f-fbf7-4275-b2be-55907fbeb18e"
-;; 
+;; :NOTE Unicly does not require downcasing the uuid b/c it already DTRT :P
 
  (defconstant +mostly-immutable-anonymous-node-namespace+ "a8c5c06f-fbf7-4275-b2be-55907fbeb18e"
    "A string of hexadecimal characters delimited by #\\- with the form: 8-4-4-12.
 Its value is the string representation of the UUID used as the value of the
 variable `*anonymous-node-namespace*'.
-:NOTE The intention is that this variable is constant should remain constant for
-across multiple vivace-graph-v2 sessions. Its value should not be mutated! 
+:NOTE The intention is that this variable should remain constant across multiple
+vivace-graph-v2 sessions. IOW its value should not be mutated!
 Doing so may invalidate the identity of pre-existing persistent UUIDs instiated
 as if by the macro `deftemplate'.")
 
@@ -26,10 +26,12 @@ as if by the macro `deftemplate'.")
 :SEE-ALSO `make-template-node-in-anonymouse-namespace'.")
 
  (defun make-template-node-in-anonymouse-namespace (stringed-node-name)
-   "Helper function for the macro `deftemplate'
+   "Helper function for the macro `deftemplate'.
 STRINGED-NODE-NAME is a string to as the NAME argument to `make-v5-uuid'.
-It is assumed that STRINGED-NODE-NAME has already have been processed with `string-downcase'."
-   (declare (special *anonymous-node-namespace*))
+It is assumed that STRINGED-NODE-NAME has already processed with `string-downcase'."
+   (declare ((or string symbol) stringed-node-name)
+            (special *anonymous-node-namespace*)
+            (uuid *anonymous-node-namespace*))
    (uuid:make-v5-uuid *anonymous-node-namespace* stringed-node-name))
 
  (defmacro tt--deftemplate (name &rest slots)
