02e7c0038acea27b3b7875d800d5a804684c847a
MON KEY <monkey@sandpframing.com>
2011-09-21 22:51:59 -0400

    Add additional type identifiers for persistent indexing and rdf semantics

--- constants.lisp
+++ constants.lisp
@@ -19,35 +19,37 @@
 (defconstant +cf-false+  -1.0)
 (defconstant +cf-unknown+ 0.0)
 
-;; :TODO These variables are treated as constants, e.g. with the +<FOO>+ convention
-;; Unless there is some reason why these shouldn't be def i'd perfer to define the as such -- MON
+;; :NOTE The `defcons'd variables are treated as constants, e.g. with the
+;; +<FOO>+ convention but defined with `cl:defparameter'.
+;; Unless there is some reason why these shouldn't be def i'd perfer to define
+;; the as such -- MON
 ;;
 ;; :NOTE Changed radix of following constants/variables to hex just in case
-;; someone has an insane value for `cl:*read-base*'
+;; someone has an insane value for `cl:*read-base*' -- MON
 
-;; :NOTE these were `cl:defparameter'd
+;; :NOTE these were `cl:defparameter'd -- MON
 ;; Shortened slot identifiers for slot keys
-(defconst +predicate-slot+     #x00)
-(defconst +subject-slot+       #x01)
-(defconst +object-slot+        #x02)
-(defconst +timestamp-slot+     #x03)
-(defconst +belief-factor-slot+ #x04)
-(defconst +deleted?-slot+      #x04)
-(defconst +derived?-slot+      #x05)
-(defconst +uuid-slot+          #x06)
-(defconst +name-slot+          #x07)
-(defconst +clauses-slot+       #x08)
-(defconst +premises-slot+      #x09)
-(defconst +conclusions-slot+   #x0a)
-(defconst +cf-slot+            #x0b)
+(defconst +predicate-slot+         #x00)
+(defconst +subject-slot+           #x01)
+(defconst +object-slot+            #x02)
+(defconst +timestamp-slot+         #x03)
+(defconst +belief-factor-slot+     #x04)
+(defconst +deleted?-slot+          #x04)
+(defconst +derived?-slot+          #x05)
+(defconst +uuid-slot+              #x06)
+(defconst +name-slot+              #x07)
+(defconst +clauses-slot+           #x08)
+(defconst +premises-slot+          #x09)
+(defconst +conclusions-slot+       #x0a)
+(defconst +cf-slot+                #x0b)
 
-;; :NOTE these were `cl:defparameter'd
+;; :NOTE These were `cl:defparameter'd -- MON
 ;; Action identifiers for serialization
-(defconst +transaction+        #x00)
-(defconst +add-triple+         #x01)
-(defconst +delete-triple+      #x02)
-(defconst +undelete-triple+    #X03)
-(defconst +set-cf+             #x04)
+(defconst +transaction+            #x00)
+(defconst +add-triple+             #x01)
+(defconst +delete-triple+          #x02)
+(defconst +undelete-triple+        #X03)
+(defconst +set-cf+                 #x04)
 
 ;; Built-in type identifiers for serializing
 (defconstant +needs-lookup+        :needs-lookup)
@@ -60,16 +62,35 @@
 (defconstant +vector+              #x07)
 (defconstant +single-float+        #x08)
 (defconstant +double-float+        #x09)
-(defconstant +ratio+               #x0A)
-(defconstant +t+                   #x0B)
-(defconstant +null+                #x0C)
-(defconstant +blob+                #x0D) ;; Uninterpreted octets
-(defconstant +dotted-list+         #x0E) 
-(defconstant +compressed-string+   #x0F)
+(defconstant +ratio+               #x0A) ;10
+(defconstant +t+                   #x0B) ;11
+(defconstant +null+                #x0C) ;12
+(defconstant +blob+                #x0D) ;13 ;; A blob is a block of uninterpreted octets 
+(defconstant +dotted-list+         #x0E) ;14 
+(defconstant +compressed-string+   #x0F) ;15
 
 ;; User-defined type identifiers for serializing. Start at 100
-(defconstant +uuid+                #x64) 
-(defconstant +triple+              #x65) 
-(defconstant +predicate+           #x66) 
-(defconstant +timestamp+           #x67) 
-(defconstant +rule+                #x68) 
+(defconstant +uuid+                #x64) ;100
+(defconstant +triple+              #x65) ;101
+(defconstant +predicate+           #x66) ;102
+(defconstant +timestamp+           #x67) ;103
+(defconstant +rule+                #x68) ;104
+
+;; Type identifiers for index persistence
+(defconstant +table+               #x6E) ;110
+(defconstant +uri+                 #x6F) ;111
+(defconstant +pathname+            #x70) ;112
+(defconstant +package+             #x71) ;113
+
+;; :NOTE Why the gap from #x72 - #x77? -- MON
+
+;; Type identifiers for rdfs semantics
+(defconstant +literal+            #x78) ;120
+(defconstant +plain-literal+      #x79) ;121
+(defconstant +typed-literal+      #x7A) ;122
+(defconstant +xml-literal+        #x7B) ;123
+(defconstant +seq+                #x7C) ;124
+(defconstant +bag+                #x7D) ;125
+(defconstant +alt+                #x7E) ;126
+(defconstant +nil+                #x7F) ;127
+
