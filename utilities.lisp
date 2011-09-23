85bb70249abe12b546f341883cd7fabd5383ca92
MON KEY <monkey@sandpframing.com>
2011-09-23 16:55:58 -0400

    Updated generic-functions `less-than' and `greater-than' with method-combinations specializing on unicly:unique-universal-identifier

--- utilities.lisp
+++ utilities.lisp
@@ -29,86 +29,164 @@
 
 (defgeneric less-than (x y)
   (:documentation "Generic less-than operator.  Allows comparison of apples and oranges.")
-  (:method ((x symbol)    (y symbol))    (string< (symbol-name x) (symbol-name y)))
-  (:method ((x symbol)    (y string))    (string< (symbol-name x) y))
-  (:method ((x symbol)    (y number))    (string< (symbol-name x) (write-to-string y)))
-
-  (:method ((x symbol)    (y uuid:uuid)) (string< (symbol-name x) 
-						  (uuid:print-bytes nil y)))
-
-  ;; (:method ((x symbol)    (y vg-uuid:vg-uuid)) (string< (symbol-name x) 
-  ;;                                                       (unicly:uuid-princ-to-string y)))
-
-  (:method ((x number)    (y number))    (< x y))
-  (:method ((x number)    (y symbol))    (string< (write-to-string x) (symbol-name y)))
-  (:method ((x number)    (y string))    (string< (write-to-string x) y))
-
-  (:method ((x number)    (y uuid:uuid)) (string< (write-to-string x) 
-						  (uuid:print-bytes nil y)))
-
-  ;; (:method ((x number)    (y vg-uuid:vg-uuid)) (<  x 
-  ;;                                                  (unicly::uuid-bit-vector-to-integer
-  ;;                                                   (unicly:uuid-to-bit-vector y))))
-
-  (:method ((x string)    (y string))    (string< x y))
-  (:method ((x string)    (y symbol))    (string< x (symbol-name y)))
-  (:method ((x string)    (y number))    (string< x (write-to-string y)))
-  (:method ((x string)    (y uuid:uuid)) (string< x (uuid:print-bytes nil y)))
-
-  ;;  (:method ((x string)    (y vg-uuid:vg-uuid)) (string< x (unicly:uuid-princ-to-string y)))
-
-  (:method ((x timestamp) (y timestamp)) (timestamp< x y))
-  (:method ((x number)    (y timestamp)) (< (timestamp-to-universal x) y))
-  (:method ((x timestamp) (y number))    (< x (timestamp-to-universal y)))
-
-  (:method ((x uuid:uuid) (y uuid:uuid)) (string< (uuid:print-bytes nil x) (uuid:print-bytes nil y)))
-
-  ;; (:method ((x vg-uuid:vg-uuid) (y vg-uuid:vg-uuid)) (string< (unicly:uuid-princ-to-string x) 
-  ;;                                                             (unicly:uuid-princ-to-string y)))
-
-  (:method ((x uuid:uuid) (y string)) (string< (uuid:print-bytes nil x) y))
-
-  ;; (:method ((x vg-uuid:vg-uuid) (y string)) (string< (unicly:uuid-princ-to-string x) y))
-
-  (:method ((x uuid:uuid) (y symbol)) (string< (uuid:print-bytes nil x) 
-					       (symbol-name y)))
-
-  ;; (:method ((x vg-uuid:vg-uuid) (y symbol)) (string< (unicly:uuid-princ-to-string nil x) 
-  ;;       				       (symbol-name y)))
-
-  (:method ((x uuid:uuid) (y number)) (string< (uuid:print-bytes nil x) 
-					       (write-to-string y)))
   
-  ;; (:method ((x vg-uuid:vg-uuid) (y number)) (< (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector x))
-  ;;       				                y))
+  (:method ((x symbol) (y symbol))
+    (string< (symbol-name x) (symbol-name y)))
+  
+  (:method ((x number) (y number))
+    (< x y))
+  
+  (:method ((x string) (y string))
+    (string< x y))
+
+  (:method ((x symbol) (y string))
+    (string< (symbol-name x) y))
+
+  (:method ((x string) (y symbol))
+    (string< x (symbol-name y)))
+
+  (:method ((x symbol) (y number))
+    (string< (symbol-name x) (write-to-string y)))
+  
+  (:method ((x number) (y symbol))
+    (string< (write-to-string x) (symbol-name y)))
+  
+  (:method ((x number) (y string))
+    (string< (write-to-string x) y))
+
+  (:method ((x string) (y number))
+    (string< x (write-to-string y)))
+
+  (:method ((x timestamp) (y timestamp)) 
+    (timestamp< x y))
+
+  (:method ((x number) (y timestamp))
+    (< (timestamp-to-universal x) y))
+  
+  (:method ((x timestamp) (y number))
+    (< x (timestamp-to-universal y)))
+
+  ;; :WAS 
+  ;; (:method ((x uuid:uuid) (y uuid:uuid)) 
+  ;;        (string< (uuid:print-bytes nil x) (uuid:print-bytes nil y)))
+  (:method ((x unicly:unique-universal-identifier) (y unicly:unique-universal-identifier)) 
+    (< (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector x))
+       (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector y))))
+
+  ;; :WAS 
+  ;; (:method ((x symbol) (y uuid:uuid))
+  ;;   (string< (symbol-name x) (uuid:print-bytes nil y)))
+  ;; (:method ((x uuid:uuid) (y symbol)) 
+  ;;   (string< (uuid:print-bytes nil x) (symbol-name y)))
+  (:method ((x symbol) (y unicly:unique-universal-identifier))
+    (string< (symbol-name x) (unicly:uuid-print-bytes-to-string y :string-or-char-type 'base-char :upcase t)))
+
+  (:method ((x unicly:unique-universal-identifier) (y symbol)) 
+    (string< (unicly:uuid-print-bytes-to-string y :string-or-char-type 'base-char :upcase t) (symbol-name y)))
+
+  ;; :WAS (:method ((x string) (y uuid:uuid))
+  ;;        (string< x (uuid:print-bytes nil y)))
+  ;;       (:method ((x uuid:uuid) (y string)) 
+  ;;         (string< (uuid:print-bytes nil x) y))
+  (:method ((x string) (y unicly:unique-universal-identifier))
+    (string< x (unicly:uuid-print-bytes-to-string y :string-or-char-type 'base-char)))
+
+  (:method ((x unicly:unique-universal-identifier) (y string))
+    (string< (unicly:uuid-print-bytes-to-string x :string-or-char-type 'base-char) y))
+
+  ;; :WAS 
+  ;; (:method ((x number) (y uuid:uuid)) 
+  ;;   (string< (write-to-string x) (uuid:print-bytes nil y)))
+  ;; (:method ((x uuid:uuid) (y number)) 
+  ;;   (string< (uuid:print-bytes nil x) (write-to-string y)))
+  (:method ((x number) (y unicly:unique-universal-identifier))
+    (<  x (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector y))))
+             
+  (:method ((x unicly:unique-universal-identifier) (y number))
+    (< (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector x)) y))
   )
 
 (defgeneric greater-than (x y)
   (:documentation "Generic greater-than operator.  Allows comparison of apples and oranges.")
-  (:method ((x symbol) (y symbol))    (string> (symbol-name x) (symbol-name y)))
-  (:method ((x symbol) (y string))    (string> (symbol-name x) y))
-  (:method ((x symbol) (y number))    (string> (symbol-name x) (write-to-string y)))
-  (:method ((x symbol) (y uuid:uuid)) (string> (symbol-name x) 
-					       (uuid:print-bytes nil y)))
-  (:method ((x number) (y number))    (> x y))
-  (:method ((x number) (y symbol))    (string> (write-to-string x) (symbol-name y)))
-  (:method ((x number) (y string))    (string> (write-to-string x) y))
-  (:method ((x number) (y uuid:uuid)) (string> (write-to-string x) 
-					       (uuid:print-bytes nil y)))
-  (:method ((x string) (y string))    (string> x y))
-  (:method ((x string) (y symbol))    (string> x (symbol-name y)))
-  (:method ((x string) (y number))    (string> x (write-to-string y)))
-  (:method ((x string) (y uuid:uuid)) (string> x (uuid:print-bytes nil y)))
-  (:method ((x timestamp) (y timestamp)) (timestamp> x y))
-  (:method ((x number)    (y timestamp)) (> (timestamp-to-universal x) y))
-  (:method ((x timestamp) (y number))    (> x (timestamp-to-universal y)))
-  (:method ((x uuid:uuid) (y uuid:uuid)) 
-    (string> (uuid:print-bytes nil x) (uuid:print-bytes nil y)))
-  (:method ((x uuid:uuid) (y string)) (string> (uuid:print-bytes nil x) y))
-  (:method ((x uuid:uuid) (y symbol)) (string> (uuid:print-bytes nil x) 
-					       (symbol-name y)))
-  (:method ((x uuid:uuid) (y number)) (string> (uuid:print-bytes nil x) 
-					       (write-to-string y))))
+  
+  (:method ((x symbol) (y symbol))
+    (string> (symbol-name x) (symbol-name y)))
+  
+  (:method ((x number) (y number))
+    (> x y))
+
+  (:method ((x timestamp) (y timestamp))
+    (timestamp> x y))
+
+  (:method ((x string) (y string))
+    (string> x y))
+  
+  (:method ((x symbol) (y string))
+    (string> (symbol-name x) y))
+  
+  (:method ((x string) (y symbol))
+    (string> x (symbol-name y)))
+  
+  (:method ((x symbol) (y number))
+    (string> (symbol-name x) (write-to-string y)))
+  
+  (:method ((x number) (y symbol)) 
+    (string> (write-to-string x) (symbol-name y)))
+
+  (:method ((x number) (y string))
+    (string> (write-to-string x) y))
+
+  (:method ((x string) (y number))
+    (string> x (write-to-string y)))
+
+  (:method ((x number) (y timestamp))
+    (> (timestamp-to-universal x) y))
+  
+  (:method ((x timestamp) (y number))
+    (> x (timestamp-to-universal y)))
+
+  ;; :WAS
+  ;; (:method ((x uuid:uuid) (y uuid:uuid)) 
+  ;;   (string> (uuid:print-bytes nil x) (uuid:print-bytes nil y)))
+  (:method ((x unicly:unique-universal-identifier) (y unicly:unique-universal-identifier)) 
+    (> (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector x))
+       (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector y))))
+  
+  ;; :WAS
+  ;; (:method ((x string) (y uuid:uuid)) 
+  ;;   (string> x (uuid:print-bytes nil y)))
+  ;; (:method ((x uuid:uuid) (y string)) 
+  ;;   (string> (uuid:print-bytes nil x) y))
+
+  (:method ((x string) (y unicly:unique-universal-identifier))
+    (string> x (unicly:uuid-print-bytes-to-string y :string-or-char-type 'base-char)))
+
+  (:method ((x unicly:unique-universal-identifier) (y string))
+    (string> (unicly:uuid-print-bytes-to-string x :string-or-char-type 'base-char) y))
+
+  ;; :WAS
+  ;; (:method ((x uuid:uuid) (y symbol)) 
+  ;;   (string> (uuid:print-bytes nil x) (symbol-name y)))
+  ;; (:method ((x symbol) (y uuid:uuid))
+  ;;   (string> (symbol-name x) (uuid:print-bytes nil y)))
+  (:method ((x symbol) (y unicly:unique-universal-identifier))
+    (string> (symbol-name x) (unicly:uuid-print-bytes-to-string y :string-or-char-type 'base-char :upcase t)))
+
+  (:method ((x unicly:unique-universal-identifier) (y symbol)) 
+    (string> (unicly:uuid-print-bytes-to-string y :string-or-char-type 'base-char :upcase t) (symbol-name y)))
+
+  ;; :WAS
+  ;; (:method ((x number) (y uuid:uuid))
+  ;;   (string> (write-to-string x) (uuid:print-bytes nil y)))           
+  ;; (:method ((x uuid:uuid) (y number)) 
+  ;;   (string> (uuid:print-bytes nil x) (write-to-string y)))
+  (:method ((x number) (y unicly:unique-universal-identifier))
+    (> x (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector y))))
+             
+  (:method ((x unicly:unique-universal-identifier) (y number))
+    (> (unicly::uuid-bit-vector-to-integer (unicly:uuid-to-bit-vector x)) y))
+  )
+             
 
 (defun uri? (string)
   (cl-ppcre:scan "^(https?|ftp)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/.*)?$" string))
